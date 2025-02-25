3cf6b305c88aeda0a57486082f977986cf2df5c4
aliulazim <aliulazimbd@gmail.com>
2025-02-26 05:10:57 +0600

    added array lenth

---
+++ JS/JS_Array/lenth.js
@@ -0,0 +1,16 @@
+const numbers = [1, 2, 3, 25, 55, 66, 77,  99];
+console.log(numbers.length);
+
+// Get elemaent value by index
+console.log(numbers);
+console.log(numbers[4]);
+
+const fourth = numbers[6];
+console.log(fourth);
+
+// Set of update element value by index
+
+numbers[0] = 897;
+console.log(numbers);
+
+
